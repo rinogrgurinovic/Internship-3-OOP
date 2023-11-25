4c8ce2ee254cc5e72354c015b64be2b2d61a06c2
rinogrgurinovic <grgurinovicrino@gmail.com>
2023-11-25 13:43:44 +0100

    Added the main menu and input

--- OOP-domaci/Program.cs
+++ OOP-domaci/Program.cs
@@ -10,15 +10,40 @@ namespace OOP_domaci
     {
         static void Main(string[] args)
         {
+            int Input()
+            {
+                bool inputVerification = true;
+                int input = 0;
+                do
+                {
+                    if (!inputVerification)
+                        Console.WriteLine("Krivi unos, pokusajte ponovno:");
+                    inputVerification = int.TryParse(Console.ReadLine(), out input);
+                    if (input > 7 || input < 1)
+                        inputVerification = false;
+                } while(!inputVerification);
+                return input;
+            }
+
             var myDictionary = new Dictionary<Contact, List<Call>>()
             {
-                {new Contact("Marin Marinovic", "0998765432", "blocked"), new List<Call> {new Call(new DateTime(2022, 12, 03, 09, 43, 55), "missed"),
+                {new Contact("Marin Marinovic", "0998765432", "blocked"), new List<Call>() {new Call(new DateTime(2022, 12, 03, 09, 43, 55), "missed"),
                                                                                           new Call(new DateTime(2023, 03, 15, 14, 32, 18), "ended") } },
-                {new Contact("Ivan Ivanovic", "0912345678", "favorite"), new List<Call> {new Call(new DateTime(2023, 11, 20, 18, 13, 46), "missed"),
+                {new Contact("Ivan Ivanovic", "0912345678", "favorite"), new List<Call>() {new Call(new DateTime(2023, 11, 20, 18, 13, 46), "missed"),
                                                                                          new Call(new DateTime(2023, 11, 25, 12, 30, 23), "ongoing")} },
-                {new Contact("Lara Laric", "0985342187", "normal"), new List<Call> {new Call(new DateTime(2021, 05, 19, 23, 54, 03), "ended"),
+                {new Contact("Lara Laric", "0985342187", "normal"), new List<Call>() {new Call(new DateTime(2021, 05, 19, 23, 54, 03), "ended"),
                                                                                     new Call(new DateTime(2021, 07, 01, 22, 03, 19), "missed")} },
             };
+
+            Console.WriteLine("1 - Ispis svih kontakata");
+            Console.WriteLine("2 - Dodavanje novih kontakata u imenik");
+            Console.WriteLine("3 - Brisanje kontakta iz imenika");
+            Console.WriteLine("4 - Editiranje preference kontakta");
+            Console.WriteLine("5 - Upravljanje kontaktom");
+            Console.WriteLine("6 - Ispis svih poziva");
+            Console.WriteLine("7 - Izlaz iz aplikacije");
+
+            int mainInput = Input();
         }
     }
 }
