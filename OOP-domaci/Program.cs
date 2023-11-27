7c65527c32f234a0471af29e6cb6e8667626ea6d
rinogrgurinovic <grgurinovicrino@gmail.com>
2023-11-27 23:23:05 +0100

    Added case 5 - case 2, changed some things in the code and finished the project

--- OOP-domaci/Program.cs
+++ OOP-domaci/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -64,18 +65,16 @@ namespace OOP_domaci
             Contact contact2 = new Contact("Ivan Ivanovic", "0912345678", "favorit");
             Contact contact3 = new Contact("Lara Laric", "0985342187", "normalan");
 
-            Call call1 = new Call(new DateTime(2022, 12, 03, 09, 43, 55), "propusten");
-            Call call2 = new Call(new DateTime(2023, 03, 15, 14, 32, 18), "propusten");
-            Call call3 = new Call(new DateTime(2023, 11, 20, 18, 13, 46), "zavrsen");
-            Call call4 = new Call(new DateTime(2023, 11, 25, 12, 30, 23), "u tijeku");
-            Call call5 = new Call(new DateTime(2021, 05, 19, 23, 54, 03), "zavrsen");
-            Call call6 = new Call(new DateTime(2021, 07, 01, 22, 03, 19), "propusten");
+            Call call1 = new Call(new DateTime(2023, 11, 20, 18, 13, 46), "zavrsen");
+            Call call2 = new Call(new DateTime(2023, 11, 25, 12, 30, 23), "propusten");
+            Call call3 = new Call(new DateTime(2021, 05, 19, 23, 54, 03), "zavrsen");
+            Call call4 = new Call(new DateTime(2021, 07, 01, 22, 03, 19), "propusten");
 
-            var myDictionary = new Dictionary<Contact, List<Call>>()
+            var phoneBook = new Dictionary<Contact, List<Call>>()
             {
-                {contact1, new List<Call>() {call1, call2 } },
-                {contact2, new List<Call>() {call3, call4 } },
-                {contact3, new List<Call>() {call5, call6 } },
+                {contact1, new List<Call>()},
+                {contact2, new List<Call>() {call1, call2 } },
+                {contact3, new List<Call>() {call3, call4 } },
             };
 
             do
@@ -92,7 +91,7 @@ namespace OOP_domaci
                 switch (mainInput)
                 {
                     case 1:
-                        foreach (var item in myDictionary)
+                        foreach (var item in phoneBook)
                             Console.WriteLine($"{item.Key.NameAndSurname} - {item.Key.PhoneNumber} - {item.Key.Preference}");
                         Console.WriteLine("Pritisnite tipku za povratak na izbornik");
                         Console.ReadKey();
@@ -107,7 +106,7 @@ namespace OOP_domaci
                             nameAndSurname = InputString();
 
                             flag = true;
-                            foreach (var item in myDictionary)
+                            foreach (var item in phoneBook)
                                 if (nameAndSurname == item.Key.NameAndSurname)
                                 {
                                     flag = false;
@@ -124,7 +123,7 @@ namespace OOP_domaci
                             phoneNumber = InputString();
 
                             flag = true;
-                            foreach (var item in myDictionary)
+                            foreach (var item in phoneBook)
                                 if (phoneNumber == item.Key.PhoneNumber)
                                 {
                                     flag = false;
@@ -146,7 +145,7 @@ namespace OOP_domaci
                         } while (!flag);
 
                         Contact newContact = new Contact(nameAndSurname, phoneNumber, preference);
-                        myDictionary.Add(newContact, new List<Call>());
+                        phoneBook.Add(newContact, new List<Call>());
 
                         Console.WriteLine("Uspjesno dodan kontakt, pritisnite tipku za povratak na izbornik");
                         Console.ReadKey();
@@ -155,7 +154,7 @@ namespace OOP_domaci
                         do
                         {
                             Console.Clear();
-                            foreach (var item in myDictionary)
+                            foreach (var item in phoneBook)
                                 Console.WriteLine($"{item.Key.NameAndSurname} - {item.Key.PhoneNumber} - {item.Key.Preference}");
 
                             if (!flag)
@@ -164,7 +163,7 @@ namespace OOP_domaci
                             phoneNumber = InputString();
 
                             flag = false;
-                            foreach (var item in myDictionary)
+                            foreach (var item in phoneBook)
                                 if (phoneNumber == item.Key.PhoneNumber)
                                 {
                                     flag = true;
@@ -183,8 +182,8 @@ namespace OOP_domaci
                             flag = true;
                             if (confirmation == "da")
                             {
-                                Contact contactToDelete = myDictionary.Keys.FirstOrDefault(c => c.PhoneNumber == phoneNumber);
-                                myDictionary.Remove(contactToDelete);
+                                Contact contactToDelete = phoneBook.Keys.FirstOrDefault(c => c.PhoneNumber == phoneNumber);
+                                phoneBook.Remove(contactToDelete);
 
                                 Console.WriteLine("Uspjesno izbrisan kontakt, pritisnite tipku za povratak na izbornik");
                                 Console.ReadKey();
@@ -203,7 +202,7 @@ namespace OOP_domaci
                         do
                         {
                             Console.Clear();
-                            foreach (var item in myDictionary)
+                            foreach (var item in phoneBook)
                                 Console.WriteLine($"{item.Key.NameAndSurname} - {item.Key.PhoneNumber} - {item.Key.Preference}");
 
                             if (!flag)
@@ -212,7 +211,7 @@ namespace OOP_domaci
                             phoneNumber = InputString();
 
                             flag = false;
-                            foreach (var item in myDictionary)
+                            foreach (var item in phoneBook)
                                 if (phoneNumber == item.Key.PhoneNumber)
                                 {
                                     flag = true;
@@ -245,7 +244,7 @@ namespace OOP_domaci
                             flag = true;
                             if (confirmation == "da")
                             {
-                                foreach (var item in myDictionary)
+                                foreach (var item in phoneBook)
                                     if (phoneNumber == item.Key.PhoneNumber)
                                     {
                                         nameAndSurname = item.Key.NameAndSurname;
@@ -253,7 +252,7 @@ namespace OOP_domaci
                                         break;
                                     }
 
-                                Contact contactToChange = myDictionary.Keys.FirstOrDefault(c => c.PhoneNumber == phoneNumber);
+                                Contact contactToChange = phoneBook.Keys.FirstOrDefault(c => c.PhoneNumber == phoneNumber);
                                 contactToChange.Preference = newPreference;
 
                                 Console.WriteLine("Uspjesno promijenjena preferenca kontakta, pritisnite tipku za povratak na izbornik");
@@ -273,7 +272,7 @@ namespace OOP_domaci
                         do
                         {
                             Console.Clear();
-                            foreach (var item in myDictionary)
+                            foreach (var item in phoneBook)
                                 Console.WriteLine($"{item.Key.NameAndSurname} - {item.Key.PhoneNumber} - {item.Key.Preference}");
 
                             if (!flag)
@@ -282,7 +281,7 @@ namespace OOP_domaci
                             phoneNumber = InputString();
 
                             flag = false;
-                            foreach (var item in myDictionary)
+                            foreach (var item in phoneBook)
                                 if (phoneNumber == item.Key.PhoneNumber)
                                 {
                                     flag = true;
@@ -299,9 +298,14 @@ namespace OOP_domaci
                             switch (subInput)
                             {
                                 case 1:
-                                    foreach (var item in myDictionary)
+                                    foreach (var item in phoneBook)
                                         if (item.Key.PhoneNumber == phoneNumber)
                                         {
+                                            if (!item.Value.Any())
+                                            {
+                                                Console.WriteLine("Ne postoji poziv s odabranim kontaktom");
+                                                break;
+                                            }
                                             foreach (var item2 in item.Value.OrderByDescending(c => c.CallingTime))
                                                 Console.WriteLine($"{item2.CallingTime} - {item2.Status}");
                                             break;
@@ -311,7 +315,45 @@ namespace OOP_domaci
                                     Console.ReadKey();
                                     break;
                                 case 2:
+                                    Contact contactToManage = phoneBook.Keys.FirstOrDefault(c => c.PhoneNumber == phoneNumber);
+                                    
+                                    if (phoneBook[contactToManage].Any(c => c.Status == "u tijeku"))
+                                    {
+                                        Console.WriteLine("Već postoji poziv u tijeku s tim kontaktom. Pričekajte da završi.");
+                                        Console.ReadKey();
+                                        break;
+                                    }
+                                    
+                                    int random = new Random().Next(0, 2);
 
+                                    Call newCall = new Call(DateTime.Now, random == 0 ? "u tijeku" : "propusten");
+                                    
+                                    if (contactToManage.Preference == "blokiran")
+                                    {
+                                        Console.WriteLine("Nemoguće uspostaviti poziv s blokiranim kontaktom.");
+                                        Console.ReadKey();
+                                        break;
+                                    }
+                                    
+                                    if (newCall.Status == "u tijeku")
+                                    {
+                                        Console.WriteLine("Poziv u tijeku");
+                                        int duration = new Random().Next(1, 21);
+                                        System.Threading.Thread.Sleep(duration * 1000);
+                                        Console.WriteLine("Poziv zavrsen");
+                                        newCall.Status = "zavrsen";
+                                        Console.ReadKey();
+                                    }
+
+                                    if (newCall.Status == "propusten")
+                                    {
+                                        Console.WriteLine("Osoba se nije javila na poziv");
+                                        Console.ReadKey();
+                                    }
+                                    
+                                    phoneBook[contactToManage].Add(newCall);
+
+                                    Console.WriteLine("Poziv uspješno dodan.");
                                     break;
                                 case 3:
                                     exit = true;
@@ -324,7 +366,7 @@ namespace OOP_domaci
                         exit = false;
                         break;
                     case 6:
-                        foreach (var item in myDictionary)
+                        foreach (var item in phoneBook)
                         {
                             if (!item.Value.Any())
                                 continue;
